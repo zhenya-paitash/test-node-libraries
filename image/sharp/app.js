8e276122cdee095150416bd72d3a4d33785fce7a
zhenya-paitash <naitavv93@gmail.com>
2020-10-03 18:36:52 +0300

    sharp

--- image/sharp/app.js
+++ image/sharp/app.js
@@ -1,10 +1,15 @@
 const sharp = require('sharp');
 const fs = require('fs');
+const axios = require('axios');
 
 (async function () {
   try {
     const image1 = 'img/original/1.jpg';
     const image2 = 'img/original/2.jpg';
+    const imageUrl = (await axios({
+      url: 'https://images.unsplash.com/photo-1601666703585-964591b026c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1700&q=80',
+      responseType: 'arraybuffer'
+    })).data;
 
     // Convert
     const imageToWebp = await sharp(image1).webp().toFile('./img/sharpes/webp.webp');
@@ -32,6 +37,9 @@ const fs = require('fs');
         background: { r: 255, g: 0, b: 0, alpha: 0.5 }
       }
     }).png().toFile('img/sharpes/create.png');
+
+    // From URL Buffer to webp file
+    await sharp(imageUrl).webp().toFile('img/sharpes/urlbuffer.webp');
 
   } catch (err) {
     console.error(err);
