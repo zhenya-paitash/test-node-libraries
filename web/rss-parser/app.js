a76d3dbf85cb172d231fc77497b3161c7ed6df07
zhenya-paitash <naitavv93@gmail.com>
2020-10-15 19:42:33 +0300

    rss-parser

--- web/rss-parser/app.js
+++ web/rss-parser/app.js
@@ -7,22 +7,25 @@ const Parser = require('rss-parser');
   const parser = new Parser();
 
   // Get from RSS feed
-  const url = 'https://www.youtube.com/feeds/videos.xml?channel_id=UC29ju8bIPH5as8OGnQzwJyA';  // traversy-media
-  const urlReddit = 'https://www.reddit.com/.rss';
-  const feed = await parser.parseURL(url); 
+  [
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UC29ju8bIPH5as8OGnQzwJyA',  // traversy-media
+    'https://www.reddit.com/.rss'  // reddit
+  ]
+  .forEach(async url => {
+    const feed = await parser.parseURL(url); 
+    let items = [];
 
-  let items = [];
+    const filename = `${feed.title.replace(/\s+/g, '-').replace(/[/\\?%*:|"<>]/g, '').toLowerCase()}.json`;
+    if (fs.existsSync(filename))
+      items = require(filename);
+    
+    await Promise.all(feed.items.map(async curItem => {
+      if (items.filter(i => i === curItem).length <= 1)
+        items.push(curItem);
+    }));
 
-  const filename = `${feed.title.replace(/\s+/g, '-').replace(/[/\\?%*:|"<>]/g, '').toLowerCase()}.json`;
-  if (fs.existsSync(filename))
-    items = require(filename);
-  
-  await Promise.all(feed.items.map(async curItem => {
-    if (items.filter(i => i === curItem).length <= 1)
-      items.push(curItem);
-  }));
-
-  // save the file
-  fs.writeFileSync(filename, JSON.stringify(items));
+    // save the file
+    fs.writeFileSync(filename, JSON.stringify(items));
+  });
 
 })();
