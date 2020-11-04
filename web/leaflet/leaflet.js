c3e22df872b19b969d90bb1ac067b0e4f41972a6
zhenya-paitash <naitavv93@gmail.com>
2020-11-04 23:39:22 +0300

    leaflet

--- web/leaflet/leaflet.js
+++ web/leaflet/leaflet.js
@@ -1,24 +1,40 @@
 navigator.geolocation.getCurrentPosition(res => {
-    // location
-    const {
-      latitude,
-      longitude,
-      accuracy
-    } = res.coords;
-      // lat = res.coords.latitude,
-      // lon = res.coords.longitude,
-      // zoom = res.coords.accuracy;
+  console.log(res);
+  // location
+  const {
+    latitude,
+    longitude,
+    accuracy
+  } = res.coords;
 
-    // инициализируем карту и установим для нее 
-    // выбранные географические координаты и уровень масштабирования
-    const map = L.map('map').setView([latitude, longitude], accuracy);
-    // const map = L.map('map').setView([lat, lon], zoom);
+  // инициализируем карту и установим для нее 
+  // выбранные географические координаты и уровень масштабирования
+  const map = L.map('map').setView([latitude, longitude], accuracy);
+  // добавим слой листов для добавления на нашу карту
+  const url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+  L.tileLayer(url, { attribution }).addTo(map);
 
-    // добавим слой листов для добавления на нашу карту
-    const url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-    L.tileLayer(url, {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-  },
-  error => console.error(error)
+  // иконка
+  const icon = L.icon({
+    iconUrl: 'iss200.png',
+    iconSize: [50, 32],
+    iconAnchor: [25, 16],
+  });
+
+  // маркер
+  const marker = L.marker([0, 0], { icon }).addTo(map);
+  marker.setLatLng([latitude, longitude])
+  .bindPopup('now you<br>here.')
+  .openPopup();
+
+  // area
+  const circle = L.circle([latitude, longitude], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.15,
+    radius: 50
+  }).addTo(map);
+  }, 
+  err => console.error(err)
 );
