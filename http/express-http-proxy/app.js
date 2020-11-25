c55cc2b96f1b0accf2ddf1e2aa9ff8ccd499722f
zhenya-paitash <naitavv93@gmail.com>
2020-11-25 23:46:11 +0300

    express-http-proxy

--- http/express-http-proxy/app.js
+++ http/express-http-proxy/app.js
@@ -1,12 +1,17 @@
+// https://webdraftt.com/tutorial/nodejs/proxy
 const
-  express = require('express'),
+  express      = require('express'),
   // Node js сервер, как и любой другой веб-сервер, может выступать в качестве прокси 
   // для всех или только части запросов. Для проксирования запросов через сервер Node js,
   // работающий в связке с фреймворком Express, используется npm модуль express-http-proxy.
-  proxy   = require('express-http-proxy'),
-  HOST    = 'localhost',
-  PORT    = 5000,
-  app     = express();
+  proxy        = require('express-http-proxy'),
+  [HOST, PORT] = ['localhost', 5000],
+  app          = express();
 
 
-app.listen();
+app.use('*', proxy('http://www.google.com', {
+  timeout: 5000
+}));
+
+
+app.listen(PORT, HOST, () => console.log(`Server run: http://${HOST}:${PORT}`));
