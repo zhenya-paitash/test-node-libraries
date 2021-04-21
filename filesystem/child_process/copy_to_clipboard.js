d331caac55d98f9e13ae842cd9bcc2a6d0a2a504
zhenya-paitash <naitavv93@gmail.com>
2021-04-21 21:58:07 +0300

    child_process upd.

--- filesystem/child_process/copy_to_clipboard.js
+++ filesystem/child_process/copy_to_clipboard.js
@@ -1,7 +1,11 @@
 const copyToBuffer = (str = 'copy from script') => {
-  const proc = require('child_process').spawn('clip');
-  proc.stdin.write(str);
-  proc.stdin.end();
+  if (process.platform === 'win32') {
+    const proc = require('child_process').spawn('clip');
+    proc.stdin.write(str);
+    proc.stdin.end();
+
+    return console.log('Copied to clipboard: Use Ctrl + V');
+  }
 };
 
 copyToBuffer(...process.argv.splice(2));
